5b4a52a5d6de5588830a367f928d54bed845f69a
Corentin-Sevestre <189099266+Corentin-Sevestre@users.noreply.github.com>
2025-05-21 17:28:17 +0200

    Ajout des fonctions tutoriels

--- src/features.h
+++ src/features.h
@@ -1,7 +1,10 @@
 #ifndef FEATURES_H
 #define FEATURES_H
-
-
+  
 void helloWorld();
-
+void dimension (char *source_path);
+void first_pixel (char *source_path); 
+void tenth_pixel (char *source_path);
+void second_line(char *source_path);
+void print_pixel(char *source_path, int x, int y);
 #endif
--- src/main.c
+++ src/main.c
@@ -1,31 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+ 
 #include <estia-image.h>
 #include <getopt.h>
-
+ 
 #include "features.h"
 #include "utils.h"
 #include "argsparse.h"
-
+ 
 int main(int argc, char **argv) {
-  /*To use debug mode: 
+  /*To use debug mode:
    - add --debug: freud.exe --debug -f images/input/image.jpeg
-   or 
+   or
    - Simply run your compiled project with start button on the blue menu bar at the bottom of the VS Code window.
    */
-
+ 
   /*DO NOT EDIT THIS PART*/
   Config configuration ;
   parse_arguments( argc, argv, &configuration ) ;
   check_debug_mode(configuration);
   check_file();
   /* END */
-
+ 
   /* Use "if ( strncmp( command, <commandname>, 9 ) == 0 )" to check if your <commandname> is called by program.*/
   /* Example with helloworld command
-   * If helloworld is a called command: freud.exe -f images/input/image.jpeg -c helloworld 
+   * If helloworld is a called command: freud.exe -f images/input/image.jpeg -c helloworld
    */
   if ( strncmp( configuration.command, "helloworld", 10 ) == 0 ) {
     /* helloworld() function is defined in feature.h and implemented in feature.c */
@@ -34,6 +34,31 @@ int main(int argc, char **argv) {
   /*
    * TO COMPLETE
    */
+  if ( strncmp( configuration.command, "dimension", 9 ) == 0 ) {
+    /* dimension() function is defined in feature.h and implemented in feature.c */
+    dimension(configuration.filenames[0]);
+  }
+
+  if ( strncmp( configuration.command, "first_pixel", 11) == 0) {
+    /* first_pixel() function is defined in feature.h and implemented in feature.c */
+    first_pixel(configuration.filenames[0]);
+  }
+
+  if ( strncmp( configuration.command, "tenth_pixel", 11) == 0) {
+    /*tenth_pixel() function is defined in feature.h and implemented in feature.c*/
+    tenth_pixel(configuration.filenames[0]);
+  }
+
+  if (strncmp(configuration.command, "second_line", 10) == 0) {
+    /* second_line() function is defined in feature.h and implemented in feature.c */
+    second_line(configuration.filenames[0]);
+  }
   
+  if (strncmp(configuration.command, "print_pixel", 10) == 0) {
+    /* print_pixel() function is defined in feature.h and implemented in feature.c */
+  print_pixel(configuration.filenames[0], atoi(configuration.arguments[0]), atoi(configuration.arguments[1]));
+  }
+
   return 0;
+  
 }
