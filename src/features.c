397e69a75de3073a3c416ce7733d61939338ab01
Matteo-Seros-ESTIA <matteo.seros@etu.estia.fr>
2025-05-21 17:28:07 +0200

    Tuto closed

--- src/features.c
+++ src/features.c
@@ -1,16 +1,93 @@
 #include <estia-image.h>
 #include <stdio.h>
-
+ 
 #include "features.h"
 #include "utils.h"
-
+ 
 /**
  * @brief Here, you have to code features of the project.
  * Do not forget to commit regurlarly your changes.
  * Your commit messages must contain "#n" with: n = number of the corresponding feature issue.
  * When the feature is totally implemented, your commit message must contain "close #n".
  */
-
+ 
 void helloWorld() {
     printf("Hello World !");
+}
+ 
+void dimension (char *source_path) {
+    int width;
+    int height;
+    int channel_count;
+    unsigned char *data;
+    int resultat = read_image_data(source_path, &data, &width, &height, &channel_count);
+    if (resultat) {
+        printf("dimension: %d, %d", width,height);
+    }
+    else {
+        printf("ERROR");
+    }
+}
+ 
+void first_pixel(char *source_path){
+    unsigned char *data;
+    int width;
+    int height;
+    int nbChannels;
+    if (read_image_data(source_path, &data, &width, &height, &nbChannels)) {
+        printf("first_pixel: %d, %d, %d\n", data[0], data[1], data[2]);
+    }
+    else {
+        printf("ERROR");
+    }
+}
+ 
+void tenth_pixel(char *source_path){
+    unsigned char *data;
+    int width;
+    int height;
+    int nbChannels;
+    if (read_image_data(source_path, &data, &width, &height, &nbChannels)) {
+        printf("tenth_pixel: %d, %d, %d", data[27], data[28], data[29]);
+    }
+    else {
+        printf("ERROR");
+    }
+}
+ 
+void second_line(char *source_path){
+    unsigned char *data;
+    int width;
+    int height;
+    int nbChannels;
+    if (read_image_data(source_path, &data, &width, &height, &nbChannels)) {
+        int pixelIndex = (width * nbChannels);
+ 
+        if (pixelIndex < width * height * nbChannels) {
+            printf("second_line: %d, %d, %d\n", data[pixelIndex], data[pixelIndex + 1], data[pixelIndex + 2]);
+        } else {
+            printf("The image does not have a second line with at least one pixel.\n");
+        }
+    }
+    else {
+        printf("ERROR");
+    }
+}
+ 
+void print_pixel(char *source_path, int x, int y){
+    int width;
+    int height;
+    int nbChannels;
+    unsigned char* data;
+    int R,G,B;
+    if (read_image_data(source_path, &data, &width, &height, &nbChannels) !=0){
+        unsigned int position = 3*(y*width+x);
+        R = data[position];
+        G = data[position+1];
+        B = data[position+2];
+        printf("print_pixel (%d, %d): %d, %d, %d", x, y, R, G, B);
+    }
+    else {
+        printf("ERROR");
+    }
 }
--- src/utils.c
+++ src/utils.c
@@ -1,9 +1,17 @@
 #include <estia-image.h>
-
+#include <stddef.h>
 #include "utils.h"
-
+ 
 /**
  * @brief Here, you have to define functions of the pixel struct : getPixel and setPixel.
- * 
+ *
  */
-
+pixelRGB* get_pixel(unsigned char *data, const unsigned int *width, const unsigned int *height, const unsigned int n, const unsigned int x, const unsigned int y) {
+  if (data == NULL || x >= *width || y >= *height) {
+    return NULL; // VÃ©rification des prÃ©conditions
+  }
+ 
+  unsigned int pixel_index = (y * (*width) + x) * n;
+ 
+  return (pixelRGB*) &data[pixel_index];
+}
