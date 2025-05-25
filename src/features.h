a3c775d97ab0dd41b28c8529dcc8ef164cba1cb9
florian <florian.signarbieux@etu.estia.fr>
2025-05-25 17:29:42 +0200

    max_pixel_feature.h

--- src/features.h
+++ src/features.h
@@ -8,7 +8,7 @@ void first_pixel (char *source_path);
 void tenth_pixel (char *source_path);
 void second_line (char *source_path);
 void print_pixel (char *source_path, int x, int y);
-
+void max_pixel (char *source_path);
 
 
 
