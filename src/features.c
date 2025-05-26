1cbc354ba7f25db36552873010866b1cae4f88b1
Corentin-Sevestre <189099266+Corentin-Sevestre@users.noreply.github.com>
2025-05-26 12:22:24 +0200

    color_blue_features.cVF

--- src/features.c
+++ src/features.c
@@ -274,4 +274,13 @@ void color_blue (char* source_path){
     int width , height, nbChannels;
     unsigned char *data;
     read_image_data(source_path, &data, &width, &height, &nbChannels);
-    
+    int x;
+    int y;
+    for (y = 0; y < height; y++){
+        for (x = 0; x < width; x++){
+            data[y*width*3 + x*3+1] = 0;
+            data[y*width*3 + x*3+2] = 0;
+        }
+    }
+    write_image_data("image_out.bmp", data, width, height);
+}
