f24bff5db47a6cf4a22bc28a5c6706a29028042d
florian <florian.signarbieux@etu.estia.fr>
2025-05-28 16:26:53 +0200

    color_green_main.c

--- src/features.h
+++ src/features.h
@@ -15,7 +15,6 @@ void min_composant (char *source_path, char component);
 void color_red (char *source_path);
 void color_blue (char *source_path);
 void color_green (char* source_path);
-
 void color_gray(char* source_path);
 
 
