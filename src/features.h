f1f1d69300b1d742b5b81886b5ae8d1a565d4939
Corentin-Sevestre <189099266+Corentin-Sevestre@users.noreply.github.com>
2025-05-30 16:47:03 +0200

    rotate_acw_features.h

--- src/features.h
+++ src/features.h
@@ -19,6 +19,7 @@ void color_gray(char* source_path);
 void rotate_cw(char *source_path);
 void mirror_horizontal(char *source_path);
 void mirror_vertical(char *source_path);
+void rotate_acw(char *source_path);
 
 
 
