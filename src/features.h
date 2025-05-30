548f7239f06793ccbe8222cb222d623d9d0eb214
florian <florian.signarbieux@etu.estia.fr>
2025-05-30 20:56:50 +0200

    rotate_cw feature.h

--- src/features.h
+++ src/features.h
@@ -20,6 +20,7 @@ void rotate_cw(char *source_path);
 void mirror_horizontal(char *source_path);
 void mirror_vertical(char *source_path);
 void rotate_acw(char *source_path);
+void rotate_cw (char *source_path);
 
 
 
