68f080d3b51130561071b6b7a6452a8a487ce229
Matteo-Seros-ESTIA <matteo.seros@etu.estia.fr>
2025-05-31 12:12:02 +0200

    mirror_vertical_features.c_complet

--- src/features.c
+++ src/features.c
@@ -425,3 +425,35 @@ void rotate_acw(char* source_path) {
         free(target_data);
     }
 }
+
+void mirror_total(char* source_path) {
+    int width, height, nbChannels;
+    unsigned char *data;
+    read_image_data(source_path, &data, &width, &height, &nbChannels);
+
+    int y, x, c;
+    unsigned char temp;
+
+    for (y = 0; y < height; y++){
+        for (x = 0; x < width/2; x++){
+            for (c = 0; c < nbChannels; c++){
+                temp = data[y*width*nbChannels + x*nbChannels + c];
+                data[y*width*nbChannels + x*nbChannels + c] = data[y*width*nbChannels + (width -x - 1)*nbChannels + c];
+                data[y*width*nbChannels + (width - x - 1)*nbChannels + c] = temp;
+            }
+        }
+    }
+
+    for (y = 0; y < height/2; y++){
+        for (x = 0; x < width; x++){
+            for (c = 0; c < nbChannels; c++){
+                temp = data[y * width * nbChannels + x * nbChannels + c];
+                data[y * width * nbChannels + x * nbChannels + c] = data[(height - y - 1) * width * nbChannels + x * nbChannels + c];
+                data[(height - y - 1) * width * nbChannels + x * nbChannels + c] = temp;
+            }
+        }
+    }
+
+    write_image_data("image_out.bmp", data, width, height);
+    free(data);
+}
