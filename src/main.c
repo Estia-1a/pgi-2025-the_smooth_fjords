e58e0f267dcaac39daeeed47735bb3ce13073289
florian <florian.signarbieux@etu.estia.fr>
2025-05-31 12:35:33 +0200

    x

--- src/main.c
+++ src/main.c
@@ -107,5 +107,8 @@ int main(int argc, char **argv) {
     /* rotate_acw() function is defined in feature.h and implemented in feature.c */
     rotate_acw(configuration.filenames[0]);
     }
+
   return 0;
 }
+
+
