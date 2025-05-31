2f7ea503a9ba49acfb476d2448d788db68fe687c
florian <florian.signarbieux@etu.estia.fr>
2025-05-31 12:48:30 +0200

    color gray luminance feature.c

--- src/features.c
+++ src/features.c
@@ -457,3 +457,24 @@ void mirror_total(char* source_path) {
     write_image_data("image_out.bmp", data, width, height);
     free(data);
 }
+
+void color_gray_luminance(char* source_path) {
+    int width, height, nbChannels;
+    unsigned char *data;
+    read_image_data(source_path, &data, &width, &height, &nbChannels);
+    
+    int y, x;
+    for (y = 0; y < height; y++) {
+        for (x = 0; x < width; x++) {
+            unsigned char red = data[y * width * 3 + x * 3];
+            unsigned char green = data[y * width * 3 + x * 3 + 1];
+            unsigned char blue = data[y * width * 3 + x * 3 + 2];
+            unsigned char gray = (unsigned char)(0.21 * red + 0.72 * green + 0.07 * blue);
+            
+            data[y * width * 3 + x * 3] = gray;
+            data[y * width * 3 + x * 3 + 1] = gray;
+            data[y * width * 3 + x * 3 + 2] = gray;
+        }
+    }
+    write_image_data("image_out.bmp", data, width, height);
+}
