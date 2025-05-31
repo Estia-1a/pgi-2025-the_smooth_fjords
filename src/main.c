45fffa833bd4279338331e007ff027c1fe2849f2
Matteo-Seros-ESTIA <matteo.seros@etu.estia.fr>
2025-05-31 11:51:55 +0200

    mirror_total_features.h_main.c

--- src/main.c
+++ src/main.c
@@ -107,5 +107,9 @@ int main(int argc, char **argv) {
     /* rotate_acw() function is defined in feature.h and implemented in feature.c */
     rotate_acw(configuration.filenames[0]);
     }
+  if (strncmp(configuration.command, "mirror_total", 12) == 0) {
+    /* mirror_total() function is defined in feature.h and implemented in feature.c */
+    mirror_total(configuration.filenames[0]);
+    } 
   return 0;
 }
