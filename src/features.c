dcf252fe9fb14bd372c362e0b96eeb1089562603
Corentin-Sevestre <189099266+Corentin-Sevestre@users.noreply.github.com>
2025-06-01 22:55:33 +0200

    color_invert_features.cVF

--- src/features.c
+++ src/features.c
@@ -530,3 +530,12 @@ void color_invert(char* source_path) {
             unsigned char red = data[y * width * 3 + x * 3];
             unsigned char green = data[y * width * 3 + x * 3 + 1];
             unsigned char blue = data[y * width * 3 + x * 3 + 2];
+            data[y * width * 3 + x * 3] = 255 - red;    // Invert red component
+            data[y * width * 3 + x * 3 + 1] = 255 - green;  // Invert green component
+            data[y * width * 3 + x * 3 + 2] = 255 - blue;   // Invert blue component
+    }
+}
+ 
+    write_image_data("image_out.bmp", data, width, height);
+    free(data);
+}
