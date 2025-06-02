236265a51c3033d42bd1861dafba287fcaedac28
Corentin-Sevestre <189099266+Corentin-Sevestre@users.noreply.github.com>
2025-06-02 11:33:04 +0200

    scale_crop_features.h

--- src/features.h
+++ src/features.h
@@ -24,6 +24,8 @@ void mirror_total(char * source_path);
 void color_gray_luminance(char* source_path);
 void color_desaturate(char* source_path);
 void color_invert(char* source_path);
+void scale_crop(char* source_path);
+
 
 
 
